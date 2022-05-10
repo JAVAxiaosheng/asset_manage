05c894a991363f39300e086d820b412b37352e02
展~ <luzhan@bilibili.com>
2022-05-10 17:57:04 +0800

    require-pro

--- src/components/employee/MaintenanceReport.vue
+++ src/components/employee/MaintenanceReport.vue
@@ -5,8 +5,12 @@
              style="background-color: #fdfdfe;margin-bottom: 15px">
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-form-item label="记录编号" style="margin-top: 20px">
-            <el-input v-model="searchForm.record_num" class="m-2" clearable filterable style="width: 200px"/>
+          <el-form-item label="记录编号" style="margin-top: 20px;margin-left: 50px">
+            <el-input v-model="searchForm.recordNum"
+                      class="m-2" clearable filterable
+                      placeholder="请贴入借用资产的记录编号"
+                      @change="search"
+                      style="width: 320px"/>
           </el-form-item>
         </el-col>
         <el-col :span="8">
@@ -14,7 +18,7 @@
         </el-col>
         <el-col :span="8">
           <el-form-item style="margin-top: 15px;margin-left:10px;">
-            <el-button type="primary" plain round>
+            <el-button type="primary" plain round @click="search">
               <el-icon style="vertical-align: middle">
                 <search/>
               </el-icon>
@@ -26,22 +30,41 @@
       </el-row>
     </el-form>
 
-    <el-table :data="tableData" border v-loading="loading" stripe style="width: 100%">
+    <el-table :data="tableData" v-loading="loading" stripe style="width: 100%">
+      <el-table-column type="expand">
+        <el-form label-position="left" v-model="propertyInfo" inline class="demo-table-expand"
+                 style="margin-left: 50px">
+          <el-form-item label="资产编号">
+            <el-tag type="info">{{ propertyInfo.property_num }}</el-tag>
+          </el-form-item>
+          <br/>
+          <el-form-item label="资产型号">
+            <el-tag>{{ propertyInfo.property_model }}</el-tag>
+          </el-form-item>
+          <br/>
+          <el-form-item label="资产类型">
+            {{ propertyInfo.property_type }}
+          </el-form-item>
+          <br/>
+          <el-form-item label="资产价格">
+            <el-tag type="danger">{{ propertyInfo.property_price + ' 元' }}</el-tag>
+          </el-form-item>
+          <br/>
+          <el-form-item label="生产厂商">
+            {{ propertyInfo.property_factory }}
+          </el-form-item>
+          <br/>
+          <el-form-item label="入库时间">
+            <el-tag type="success">{{ formatDate(propertyInfo.create_date) }}</el-tag>
+          </el-form-item>
+        </el-form>
+      </el-table-column>
       <el-table-column prop="record_num" label="记录编号" min-width="120px" align="center">
         <template v-slot="props">
           {{
             props.row.record_num ?
                 props.row.record_num.replace(/([A-Za-z0-9]{3})([A-Za-z0-9]{25})([A-Za-z0-9]{4})/, "$1****$3") : ""
           }}
-          <el-tooltip
-              class="box-item"
-              effect="customized"
-              content="点击记录编号"
-              placement="top-start">
-            <el-icon @click="copyRecordNum(props.row)">
-              <document-copy/>
-            </el-icon>
-          </el-tooltip>
         </template>
       </el-table-column>
       <el-table-column align="center" prop="property_num" label="资产编号" min-width="90px"/>
@@ -63,44 +86,12 @@
           <el-tag type="warning" v-if="props.row.inout_state === 2">借用</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="操作" fixed="right" width="110px">
-        <template v-slot="props">
-          <el-button type="primary" plain round
-                     size="small"
-                     @click="returnBack(props.row)"
-                     v-if="props.row.inout_state === 2 && props.row.employee_name === username">
-            归还
-          </el-button>
-          <el-popconfirm
-              confirm-button-text="确定"
-              cancel-button-text="取消"
-              @confirm="deleteUseRecord(props.row)"
-              title="确定要删除此条记录么?">
-            <template #reference>
-              <el-button type="danger" plain circle size="small">
-                <el-icon style="vertical-align: middle">
-                  <delete/>
-                </el-icon>
-              </el-button>
-            </template>
-          </el-popconfirm>
-        </template>
+      <el-table-column align="center" label="操作" fixed="right" width="140px">
+        <el-button type="primary" plain>
+          维修上报
+        </el-button>
       </el-table-column>
     </el-table>
-
-    <!--    分页-->
-    <el-pagination
-        background
-        class="pageInput"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        v-model:currentPage="pageNum"
-        v-model:page-size="pageSize"
-        :page-sizes="[5,10, 15, 20, 50]"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-        style="margin-top: 8px;float: right;background-color: #fdfdfe">
-    </el-pagination>
   </div>
 </template>
 
@@ -111,52 +102,58 @@ export default {
   name: "MaintenanceReport",
   data() {
     return {
+      loading: false,
       searchForm: {
-        record_num: ""
+        recordNum: ""
       },
-      tableData:[],
+      tableData: [],
       pageNum: 1,
-      pageSize: 10,
-      total: 0,
-
+      pageSize: 1,
+      propertyInfo: {}
     }
   },
   mounted() {
-    this.listUseRecord();
   },
-  methods:{
-    // 复制记录编号
-    copyRecordNum(row) {
-      let input = document.createElement('input')
-      input.value = row.record_num;
-      document.body.appendChild(input)
-      input.select()
-      document.execCommand('copy')
-      this.$message.success('复制成功')
-      document.body.removeChild(input);
+  methods: {
+    search() {
+      if (this.searchForm.recordNum === '') {
+        this.tableData = [];
+        this.propertyInfo = {};
+        return
+      }
+      this.listUseRecord();
     },
     // 格式化日期
     formatDate(time) {
       return moment(time * 1000).format("YYYY-MM-DD HH:mm");
     },
+    // 查询资产信息
+    listProInfo(propertyNum) {
+      let params = {
+        page_num: this.pageNum,
+        page_size: this.pageSize,
+        property_num: propertyNum
+      };
+      this.$http.get('api/property_info/query_property', {params}).then((resp) => {
+        let apiData = resp.data;
+        this.propertyInfo = apiData.data[0];
+      });
+    },
     listUseRecord() {
       this.loading = true;
       let params = {
         page_num: this.pageNum,
-        page_size: this.pageSize
+        page_size: this.pageSize,
+        record_num: this.searchForm.recordNum.trim()
       };
-      // if (this.searchForm.property_name !== '') {
-      //   params['property_num'] = this.searchForm.property_name
-      // }
-      // if (this.searchForm.employee_num !== '') {
-      //   params['employee_num'] = this.searchForm.employee_num
-      // }
       this.$http.get('api/inout_record/query_inout_record', {params}).then(resp => {
         let apiData = resp.data;
         if (apiData.code === 0) {
+          if (apiData.data.length !== 0) {
+            this.listProInfo(apiData.data[0].property_num);
+          }
           setTimeout(() => {
             this.tableData = apiData.data;
-            this.total = apiData.total;
             this.loading = false;
           }, 500);
         } else {
@@ -164,15 +161,6 @@ export default {
           this.$message.error("查询记录接口错误")
         }
       });
-    },
-
-    handleSizeChange(val) {
-      this.pageSize = val;
-      this.listUseRecord();
-    },
-    handleCurrentChange(val) {
-      this.pageNum = val;
-      this.listUseRecord();
     },
   }
 }
